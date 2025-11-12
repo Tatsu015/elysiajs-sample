7227cd93aeb216a46d9c02f2140c540f0058822c
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-11-13 00:00:00 +0900

    add openapi

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from 'elysia'
+import { openapi } from '@elysiajs/openapi'
 import { node } from '@elysiajs/node'
 
 const app = new Elysia({ adapter: node() })
+  .use(openapi())
   .get('/', () => 'Hello Elysia')
   .listen(3000)
