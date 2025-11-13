8295b129f9f442f09d5023956d4562c238e04921
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-11-13 23:39:21 +0900

    Initial commit (via bun create)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { Elysia } from 'elysia'
-import { openapi } from '@elysiajs/openapi'
-import { node } from '@elysiajs/node'
+import { Elysia } from "elysia";
 
-const app = new Elysia({ adapter: node() })
-  .use(openapi())
-  .get('/', () => 'Hello Elysia')
-  .listen(3000)
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
