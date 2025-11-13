c7120c18676a0f1d7c1c0d90a6830d7de0184059
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-11-13 23:41:45 +0900

    add openapi setting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
+import { openapi } from '@elysiajs/openapi'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+new Elysia()
+  .use(openapi())
+  .get('/', () => 'hello')
+  .post('/hello', () => 'OpenAPI')
+  .listen(3000)
